1e8d46bfc00e3856f739b26cc1e429f46badf2d5
jo285317 <37353455+jo285317@users.noreply.github.com>
2020-02-27 21:00:02 -0500

    Update vm_setup.sh

--- scripts/vm_setup.sh
+++ scripts/vm_setup.sh
@@ -5,22 +5,13 @@ set -x
 sudo apt-get update
 
 # dependencies for building axtls, toybox, fiasco
-yes | sudo apt-get install python python-pip python-dev build-essential make gcc libreadline-dev libselinux1-dev libssl-dev libncurses5-dev patch liblua50-dev libpam0g-dev libdmalloc-dev electric-fence libdlib-dev libaudit-dev linux-source-4.4.0 g++-mips-linux-gnu cbmc cppcheck default-jdk
-yes | sudo apt-get install libelf-dev build-essential libncurses-dev bison flex libssl-dev bc lzop
-
-# smack dependencies for toybox
-yes | sudo apt-get install autoconf libtool-bin
-yes | sudo apt-get install python python-pip python-setuptools python-dev build-essential python3-pip
-sudo pip install kmaxtools
-sudo pip3 install scipy
+yes | sudo apt-get install go wget xz-utils cmake python autoconf automake freetype-dev linux-headers
+yes | sudo apt-get install make g++ git zlib1g-dev libncurses5-dev libssl-dev libpcre2-dev zip vim
+yes | sudo apt-get install python python3 python-pip python-dev build-essential make gcc libreadline-dev libselinux1-dev libssl-dev libncurses5-dev patch liblua50-dev libpam0g-dev libdmalloc-dev electric-fence libdlib-dev libaudit-dev linux-source-4.4.0 g++-mips-linux-gnu cbmc cppcheck default-jdk
 
 
-# cross-compilation tools for fiasco
 
 # dependencies for building linux
-
-# dependencies for java program to extract gcc args
-# sudo apt-get install openjdk-8-jre-headless
 
 # allow user to add to /usr/local
 sudo chgrp -R vagrant /usr/local; sudo chmod -R g+w /usr/local
@@ -35,13 +26,31 @@ echo 'export PATH=/usr/local/bin:$PATH' >> /home/vagrant/.bash_profile
 
 # get source code and setup repos
 cd /home/vagrant
-#git clone git://git.kernel.org/pub/scm/linux/kernel/git/next/linux-next.git
-#cd linux-next
-#git checkout next-20191211
-#wget https://kmaxtools.opentheblackbox.net/formulas/kmax-formulas_linux-next-20191211.tar.bz2
-#tar -xvf kmax-formulas_linux-next-20191211.tar.bz2
 
+#set-up llvm
+wget http://llvm.org/releases/9.0.0/llvm-9.0.0.src.tar.xz
+wget http://llvm.org/releases/9.0.0/cfe-9.0.0.src.tar.xz
+tar xf llvm-9.0.0.src.tar.xz
+tar xf cfe-9.0.0.src.tar.xz
+mv cfe-9.0.0.src llvm-9.0.0.src/tools/clang
+mkdir llvm-9.0.0.obj
+cd llvm-9.0.0.obj
+cmake -DCMAKE_BUILD_TYPE=MinSizeRel ../llvm-9.0.0.src 
+make -j32  
+cd /home/vagrant
+mkdir llvm-9.0.0.dbj
+cd llvm-9.0.0.dbj
+cmake -DCMAKE_BUILD_TYPE:STRING=Debug ../llvm-9.0.0.src 
+make -j32
+cd /home/vagrant
+git clone https://github.com/SVF-tools/SVF.git SVF
+cd SVF
+LLVMRELEASE=/home/vagrant/llvm-9.0.0/llvm-9.0.0.obj
+LLVMDEBUG=/home/vagrant/llvm-9.0.0/llvm-9.0.0.dbg
 
+bash ./build.sh
+#bash ./build.sh debug
+cd /home/vagrant
 # Copy README and license info
 cp /vagrant/.vagrant_resources/* ~
 
