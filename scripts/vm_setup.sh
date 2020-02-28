b150afe93a955016f8ce1fa7a462040a77d7374b
jo285317 <37353455+jo285317@users.noreply.github.com>
2020-02-27 21:06:03 -0500

    Update vm_setup.sh

--- scripts/vm_setup.sh
+++ scripts/vm_setup.sh
@@ -45,10 +45,16 @@ make -j32
 cd /home/vagrant
 git clone https://github.com/SVF-tools/SVF.git SVF
 cd SVF
-LLVMRELEASE=/home/vagrant/llvm-9.0.0/llvm-9.0.0.obj
-LLVMDEBUG=/home/vagrant/llvm-9.0.0/llvm-9.0.0.dbg
+export LLVM_SRC=/home/vagrant/llvm-9.0.0/llvm-9.0.0.src
+export LLVM_OBJ=/home/vagrant/llvm-9.0.0/llvm-9.0.0.obj
+export LLVM_DIR=/home/vagrant/llvm-9.0.0/llvm-9.0.0.obj
+export PATH=$LLVM_DIR/bin:$PATH
 
-bash ./build.sh
+mkdir Release-build
+cd Release-build
+cmake ../
+make -j4
+
 #bash ./build.sh debug
 cd /home/vagrant
 # Copy README and license info
@@ -56,3 +62,55 @@ cp /vagrant/.vagrant_resources/* ~
 
 # Force vagrant to read ~/.bashrc
 echo "source ~/.bashrc" >> /home/vagrant/.bash_profile
+
+
+# setup svf
+
+
+export LLVM_OBJ_ROOT=/home/vagrant/llvm-9.0.0/llvm-9.0.0.obj
+
+export PATH=$LLVM_OBJ_ROOT/bin:$PATH
+export LLVM_DIR=$LLVM_OBJ_ROOT
+#export LLVM_OBJ_ROOT=$LLVM_HOME/llvm-$llvm_version.dbg
+#export PATH=$LLVM_OBJ_ROOT/Debug+Asserts/bin:$PATH
+export LLVMOPT=opt
+export CLANG=$LLVM_OBJ_ROOT/bin/clang
+export CLANGCPP=$LLVM_OBJ_ROOT/bin/clang++
+export LLVMDIS=llvm-dis
+export LLVMLLC=llc
+
+##############astyle code formatting###############
+AstyleDir=/home/ysui/astyle/build/clang
+export PATH=$AstyleDir/bin:$PATH
+
+##############check what os we have
+PLATFORM='unknown'
+unamestr=`uname`
+if [[ "$unamestr" == 'Linux' ]]; then
+export PLATFORM='linux'
+elif [[ "$unamestr" == 'Darwin' ]]; then
+export PLATFORM='darwin'
+elif [[ "$unamestr" == 'FreeBSD' ]]; then
+export PLATFORM='freebsd'
+fi
+
+
+#########PATH FOR PTA##############                                                                 
+export SVF_HOME=`pwd`
+if [[ $1 == 'debug' ]]
+then
+PTAOBJTY='Debug'
+else
+PTAOBJTY='Release'
+fi
+Build=$PTAOBJTY'-build'
+export SVF_HOME=`pwd`
+export PTABIN=$SVF_HOME/$Build/bin
+export PTALIB=$SVF_HOME/$Build/lib
+export PTARTLIB=$SVF_HOME/lib/RuntimeLib
+export PATH=$PTABIN:$PATH
+
+export PTATEST=$SVF_HOME/PTABen
+export PTATESTSCRIPTS=$PTATEST/scripts
+export RUNSCRIPT=$PTATESTSCRIPTS/run.sh
+
